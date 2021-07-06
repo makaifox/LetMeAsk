071733c86bc3db28c219d437842affdc982a9a98
makaifox <yu.trindade@gmail.com>
2021-07-06 10:19:32 -0300

    Updates

--- src/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { AuthContext } from '../contexts/AuthContexts'
-
-export function useAuth() {
-  const value = useContext(AuthContext)
-
-  return value;
-}
